0657775fc25913d8e76305be1e2af193244b3722
aspita <49107833+aspita@users.noreply.github.com>
2026-06-04 10:31:52 +0100

    add service worker

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'safetrack-v1';
+const FILES = [
+  '/home.html',
+  '/index.html',
+  '/scanner.html',
+  '/pin-generator.html',
+  '/pin-verify.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
